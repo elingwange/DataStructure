8ff3de458bb11a3f121aceff8bc8cc4a4f9dca81
fangyan <fangyan123abc>
2020-08-14 20:29:31 +0800

    [init]初始项目

---
+++ DataStructure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DataStructure
+//
+//  Created by iMac on 2020/8/14.
+//  Copyright © 2020 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
